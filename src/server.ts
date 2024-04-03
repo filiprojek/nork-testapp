726fbd5ae3a85cfc33f972d167598a39870e0871
Filip Rojek <filip@filiprojek.cz>
2024-04-04 01:38:43 +0200

    Environment module tested

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import http from 'http'
+import path from 'path'
 import { app } from './app'
 //import env from './config/environment'
 //import { Succ } from './services/globalService'
 //import database from './config/database'
-import {Log, Env as env} from 'nork'
+import {Log, Env} from 'nork'
 //const env = {
 //	APP_PORT: 9000,
 //	APP_HOSTNAME: 'localhost'
 //}
+const env = Env.get(path.resolve(process.env.NODE_ENV ? `../.env.${process.env.NODE_ENV}` : 'src/.env'))
+console.log(env)
+
 const port: number = env.APP_PORT || 8080
 const hostname: string = env.APP_HOSTNAME || 'localhost'
 export const server = http.createServer(app)
